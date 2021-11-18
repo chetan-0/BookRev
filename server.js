6f0010623ad2f5b4435efc81fd36527a6d9f9b70
Chetan <vipershiraguppi@gmail.com>
2021-11-18 11:58:28 +0530

    node server added

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path');
+
+const app = express()
+
+const u = bodyParser.urlencoded({extended:false})
+
+app.use(express.static(path.join(__dirname,'html')))
+app.use(express.static(path.join(__dirname,'css')))
+app.use(express.static(path.join(__dirname,'assets')))
+
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+
+app.listen(3000,()=>{
+    console.log('Server Running!')
+})
