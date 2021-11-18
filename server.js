f503ca461751db5055debb2af0c5b7732c084849
Chetan <vipershiraguppi@gmail.com>
2021-11-18 12:01:18 +0530

    node server added

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require('path');
 
 const app = express()
 
-const u = bodyParser.urlencoded({extended:false})
+const u = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static(path.join(__dirname,'html')))
-app.use(express.static(path.join(__dirname,'css')))
-app.use(express.static(path.join(__dirname,'assets')))
+app.use(express.static(path.join(__dirname, 'html')))
+app.use(express.static(path.join(__dirname, 'css')))
+app.use(express.static(path.join(__dirname, 'assets')))
 
-app.use('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'))
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server Running!')
 })
