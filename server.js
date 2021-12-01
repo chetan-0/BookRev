617b1e24d55421be8c98774c79a218f3f9b949c9
BinayakB <bishnu.binayak12@gmail.com>
2021-12-01 11:10:47 +0530

    bookpages

--- server.js
+++ server.js
@@ -55,13 +55,37 @@ app.post('/log', u, (req, res) => {
     })
 })
 
-// function gopage() {
-//     console.log("hi");
+// app.post('/reg', u, function (req, res) {
+//     var response = {
+//         name: req.body.signupmail,
+//         password: req.body.signuppass
+//     }
+//     MongoClient.connect('mongodb://localhost:27017/', function (err, db) {
+//         if (err)
+//             throw err;
+//         else
+//             console.log("mongo connected!")
+//         var dbo = db.db("base1");
+//         dbo.collection('users').insertOne(response);
+//     })
+//     res.sendFile(path.join(__dirname, 'index.html'))
+// })
+
 app.get('/rememberme', (req, res) => {
-    // res.sendFile(__dirname + "/fiction_rememberme.html")
+    // console.log(req.body.remembermebtn);
+    // console.log(res);
     res.sendFile(path.join(__dirname, './html/explore_subpages/fiction_rememberme.html'))
 })
-// }
+app.get('/alchemist', (req, res) => {
+    // console.log(req);
+    // console.log(res);
+    res.sendFile(path.join(__dirname, './html/explore_subpages/fiction_alchemist.html'))
+})
+app.get('/writtenstars', (req, res) => {
+    // console.log(req);
+    // console.log(res);
+    res.sendFile(path.join(__dirname, './html/explore_subpages/fiction_stars.html'))
+})
 
 app.listen(3000, () => {
     console.log('BookRev server running!')
