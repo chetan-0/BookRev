5180fd89492ecc657de35cc2c4823dea14807f04
BinayakB <bishnu.binayak12@gmail.com>
2021-12-04 08:59:20 +0530

    custom merge sessions

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(express.static(path.join(__dirname, 'css')))
 app.use(express.static(path.join(__dirname, 'assets')))
 
 app.get('/', (req, res) => {
-
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
